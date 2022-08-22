84d10469c656b5c7aaf998195bd97c2feea9df07
Wei Zang <richzw@gmail.com>
2022-08-22 12:55:28 +0800

    feat(snippet): update event bus

--- snippet/event_bus.go
+++ snippet/event_bus.go
@@ -65,3 +65,47 @@ func (bus *AsyncEventBus) Publish(topic string, args ...interface{}) {
 		go handlers[i].Call(params)
 	}
 }
+
+// Ex2: Use channel
+type DataEvent struct {
+	Data  interface{}
+	Topic string
+}
+
+// DataChannel is a channel which can accept an DataEvent
+type DataChannel chan DataEvent
+
+// DataChannelSlice is a slice of DataChannels
+type DataChannelSlice []DataChannel
+
+// EventBus stores the information about subscribers interested for a particular topic
+type EventBus struct {
+	subscribers map[string]DataChannelSlice
+	rm          sync.RWMutex
+}
+
+func (eb *EventBus) Publish(topic string, data interface{}) {
+	eb.rm.RLock()
+	if chans, found := eb.subscribers[topic]; found {
+		// this is done because the slices refer to same array even though they are passed by value
+		// thus we are creating a new slice with our elements thus preserve locking correctly.
+		// special thanks for /u/freesid who pointed it out
+		channels := append(DataChannelSlice{}, chans...)
+		go func(data DataEvent, dataChannelSlices DataChannelSlice) {
+			for _, ch := range dataChannelSlices {
+				ch <- data
+			}
+		}(DataEvent{Data: data, Topic: topic}, channels)
+	}
+	eb.rm.RUnlock()
+}
+
+func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
+	eb.rm.Lock()
+	if prev, found := eb.subscribers[topic]; found {
+		eb.subscribers[topic] = append(prev, ch)
+	} else {
+		eb.subscribers[topic] = append([]DataChannel{}, ch)
+	}
+	eb.rm.Unlock()
+}
