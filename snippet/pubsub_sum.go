f8cead8fc96da5f5310054c4f5032d78b59d4ca7
Wei Zang <richzw@gmail.com>
2022-11-23 21:00:17 +0800

    feat(snippet): update pubsub sum

--- snippet/pubsub_sum.go
+++ snippet/pubsub_sum.go
@@ -1,4 +1,70 @@
 package snippet
 
+import "fmt"
+
 // 1. interface-and-switch
 // Source: https://github.com/WillSewell/go-sum-types-alternatives
+
+type event interface {
+	visit(v eventVisitor)
+}
+
+// The handlers for each event type are defined in instances of this struct
+type eventVisitor struct {
+	// Notice these can have different function signatures
+	visitSubscribe func(subscribeEvent)
+	visitPublish   func(publishEvent)
+}
+
+type subscribeEvent struct {
+	messageChan chan<- string
+}
+
+func (s subscribeEvent) visit(v eventVisitor) {
+	v.visitSubscribe(s)
+}
+
+type publishEvent struct {
+	message string
+}
+
+func (p publishEvent) visit(v eventVisitor) {
+	v.visitPublish(p)
+}
+
+type pubsubBus struct {
+	subs      []chan<- string
+	eventChan chan event
+}
+
+func (p *pubsubBus) run() {
+	for event := range p.eventChan {
+		// Handler implementations are passed in here.
+		// Alternative handler implementations could be defined by
+		// creating an alternative version of this method.
+		event.visit(eventVisitor{
+			visitSubscribe: p.handleSubscribe,
+			visitPublish:   p.handlePublish,
+		})
+	}
+}
+
+func (p *pubsubBus) handleSubscribe(subscribeEvent subscribeEvent) {
+	p.subs = append(p.subs, subscribeEvent.messageChan)
+}
+
+func (p *pubsubBus) handlePublish(publishEvent publishEvent) {
+	for _, sub := range p.subs {
+		sub <- publishEvent.message
+	}
+}
+
+func main() {
+	bus := pubsubBus{make([]chan<- string, 0), make(chan event)}
+	go bus.run()
+	messageChan := make(chan string, 1)
+	bus.eventChan <- subscribeEvent{messageChan}
+	bus.eventChan <- publishEvent{"message"}
+	msg := <-messageChan
+	fmt.Println("Received:", msg)
+}
