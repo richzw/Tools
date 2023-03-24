a2b6f9f3a6b8deb32f59fbbc283cacc6dd4f101b
Wei Zang <richzw@gmail.com>
2023-03-24 12:49:32 +0800

    feat(snippet): update code

--- snippet/consumer_producer.go
+++ snippet/consumer_producer.go
@@ -50,3 +50,78 @@ func prodCons() {
 	close(ch)
 	wc.Wait()
 }
+
+// without waitgroup
+
+const producerCount int = 3
+const consumerCount int = 3
+
+var workers []*producers
+
+type producers struct {
+	myQ  chan string
+	quit chan bool
+	id   int
+}
+
+func execute(jobQ chan<- string, workerPool chan *producers, allDone chan<- bool) {
+	for _, j := range messages {
+		jobQ <- j
+	}
+	close(jobQ)
+	for _, w := range workers {
+		w.quit <- true
+	}
+	close(workerPool)
+	allDone <- true
+}
+
+func produce(jobQ <-chan string, p *producers, workerPool chan *producers) {
+	for {
+		select {
+		case msg := <-jobQ:
+			{
+				workerPool <- p
+				if len(msg) > 0 {
+					fmt.Printf("Job \"%v\" produced by worker %v\n", msg, p.id)
+				}
+				p.myQ <- msg
+			}
+		case <-p.quit:
+			return
+		}
+	}
+}
+
+func consume(cIdx int, workerPool <-chan *producers) {
+	for {
+		worker := <-workerPool
+		if msg, ok := <-worker.myQ; ok {
+			if len(msg) > 0 {
+				fmt.Printf("Message \"%v\" is consumed by consumer %v from worker %v\n", msg, cIdx, worker.id)
+			}
+		}
+	}
+}
+
+func test() {
+	jobQ := make(chan string)
+	allDone := make(chan bool)
+	workerPool := make(chan *producers)
+
+	for i := 0; i < producerCount; i++ {
+		workers = append(workers, &producers{
+			myQ:  make(chan string),
+			quit: make(chan bool),
+			id:   i,
+		})
+		go produce(jobQ, workers[i], workerPool)
+	}
+
+	go execute(jobQ, workerPool, allDone)
+
+	for i := 0; i < consumerCount; i++ {
+		go consume(i, workerPool)
+	}
+	<-allDone
+}
